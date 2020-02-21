9f31bf8b5685595efd7c156122d3446beef8c738
hrisDelisyarov <59515776+hrisDelisyarov@users.noreply.github.com>
2020-02-21 17:11:38 +0200

    Update MaximumElement.java

--- stacksAndQueues/MaximumElement.java
+++ stacksAndQueues/MaximumElement.java
@@ -3,7 +3,13 @@ package stacksAndQueues;
 import java.util.ArrayDeque;
 import java.util.Collections;
 import java.util.Scanner;
+/*
+You have an empty sequence, and you will be given N commands. Each command is one of the following types:
+⦁	"1 X" - Push the element X into the stack.
+⦁	"2" - Delete the element present at the top of the stack.
+⦁	"3" - Print the maximum element in the stack.
 
+*/
 public class MaximumElement {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
